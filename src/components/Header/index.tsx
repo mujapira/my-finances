7d667a36e7e0ad1858555e0afd4f620be3632bb7
Mujapira <malmasmello2@gmail.com>
2022-08-25 14:27:16 -0300

    feat: Radix modal structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,25 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import Logo from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <NewTransactionButton>Nova transação</NewTransactionButton>
         <img src={Logo} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
